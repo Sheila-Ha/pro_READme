9fee5d0a87405d3b85b83233e23db5eb7e929521
sheilahanson <slhanson11@live.com>
2024-01-04 14:34:40 -0600

    ran in node index.js and works

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
+
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
+// const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [
-{
-  name: 'Title',
-  content: 'What is the name of your project?',
-},
-{
-  name: '<Motivation',
-  content: 'What was your motivation?',
-},
-{
-  name: 'Why',
-  content: 'Why did you build this project?',
-},
-{
-  name: 'Problem Solve',
-  content: 'What problem does it solve?',
-},
-{
-  name: 'Learned',
-  content: 'What did you learn?',
-},
-{
-  name: 'Shine',
-  content: 'What makes your project stand out?',
-},
-{
-  type: 'checkbox',
-  name: 'Contents',
-  message: 'Would you like to include a Table of Contents?',
-  choices: ['yes', 'no'],
-},
-{
-  type: 'Checkbox',
-  name: 'Contents',
-  message: 'Which ones would you like to add?',
-  choices: ['Installation', 'Usage', 'Credits', 'License', 'Badges', 'Features', 'How to Contribute', 'Tests'],
-},
+// const questions = [
+inquirer
+  .prompt([
+    {
+      name: 'Title',
+      content: 'What is the name of your project?',
+    },
+    {
+      name: '<Motivation',
+      content: 'What was your motivation?',
+    },
+    {
+      name: 'Why',
+      content: 'Why did you build this project?',
+    },
+    {
+      name: 'Problem Solve',
+      content: 'What problem does it solve?',
+    },
+    {
+      name: 'Learned',
+      content: 'What did you learn?',
+    },
+    {
+      name: 'Shine',
+      content: 'What makes your project stand out?',
+    },
+    {
+      type: 'Checkbox',
+      name: 'Contents',
+      choices: ['yes', 'no'],
+      message: 'Would you like to include a Table of Contents?',
+    },
+    {
+      type: 'Checkbox',
+      name: 'Contents',
+      choices: ['Installation', 'Usage', 'Credits', 'License', 'Badges', 'Features', 'How to Contribute', 'Tests'],
+      message: 'Which ones would you like to add?',
+    },
+  ])
+    .then((responses) => {
+      console.log('responses :>> ', responses);
+      const filename = `${responses.name.toLowerCase()}json`;
 
-
-];
+      fs.writeFile(filename, JSON.stringify(responses, null, 2), (err) =>
+      err ? console.error(err) : console.log('Success')
+      );
+    })
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
-
-// Function call to initialize app 
-init();
