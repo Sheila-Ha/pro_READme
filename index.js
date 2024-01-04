bf5572a885290ff1118746f11ff8afa8a36efb60
sheilahanson <slhanson11@live.com>
2024-01-04 13:33:35 -0600

    added all questions for user input

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+  What was your motivation?
+  Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+  What problem does it solve?
+  What did you learn?
+  What makes your project stand out?
+  Table of Contents 
+
+  [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  ## Badges
+  ## Features
+  How to Contribute
+  ## Tests
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
