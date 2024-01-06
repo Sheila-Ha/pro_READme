94838917acd76482d801eb27fb7f4bd4bfddab34
sheilahanson <slhanson11@live.com>
2024-01-06 13:27:02 -0600

    updated gen markdown js and changed inquirer prompts

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const fs = require('fs');
 const inquirer = require('inquirer');
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 
 // TODO: Create an array of questions for user input
@@ -14,44 +16,36 @@ inquirer
       message: 'What is the title of your project?',
     },
     {
-      name: 'your motivation',
-      message: 'What was your motivation?',
-    },
-    {
-      name: 'why',
-      message: 'Why did you build this project?',
-    },
-    {
-      name: 'problem solve',
-      message: 'What problem does it solve?',
-    },
-    {
-      name: 'learned',
-      message: 'What did you learn?',
-    },
-    {
-      name: 'shine',
-      message: 'What makes your project stand out?',
-    },
-    {
-      type: 'checkbox',
-      name: 'contents',
-      choices: ['yes', 'no'],
-      message: 'Would you like to include a Table of Contents?',
+      type: 'input',
+      name: 'description',
+      message: 'What is '
     },
     {
       type: 'checkbox',
       name: 'table of content',
-      choices: ['Installation', 'Usage', 'Credits', 'License', 'Badges', 'Features', 'How to Contribute', 'Tests'],
+      choices: ['Installation', 'Usage', 'Features', 'Contributing', 'License', 'Badges', 'Tests'],
       message: 'Which ones would you like to add?',
     },
     {
+      type: 'input',
       name: 'user story',
       message: 'What is your User Story?',
     },
     {
+      type: 'input',
       name: 'acceptance criteria',
       message: "What is your Acceptance Criteria?",
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'List technologies used with this project',
+    }
+    {
+      type: 'checkbox',
+      name: 'license',
+      message: 'Select a license for your project',
+      choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD3", "GNU3", "none"],
     }
   ])
     .then((responses) => {
@@ -65,46 +59,17 @@ inquirer
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+  return fs.watchFileSync(path.join(process.cwd(), fileName), data);
+}
   console.log(filename);
   console.log(data);
 
-  //verify if a readme has been made 
-  //new file = write file
-  //append to exit file
-  fs.appendFile(filename, function(err) {
-    if (err) {
-      console.error(err);
-    }
-      console.log("Success, Your README.md file has been generated")
-  });
-}
-
-
-
-
 // TODO: Create a function to initialize app
-async function init() {
-  try {
-    // prompt inquirer questions
-    const userResponses = await inquirer.prompt(questions);
-    console.log("Your responses: ", userResponses);
-    console.log("Thank you for your time");
-
-    //requests github api for user info
-    const userInfo = await appendFile.getUser(userResponses);
-    console.log("Github user info: ", userInfo);
-
-    //send inquirer userResponses and github userInfo to generateMarkdown
-    console.log("Genereate your README")
-    const markdown = generateMarkdown(userResponses, userInfo);
-    console.log(markdown);
-
-    //wire markdown to file
-    await writerFileAsync('ExampleREADME.md', markdown);
-
-  } catch (error) {
-      console.log(error);
-  }
-};
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    console.log("Creating Professional README.md File...");
+    writeToFile(".dist/READ.md", generateMarkdown({ ...responses }));
+});
+}
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,75 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "none") {
+    return `/n* [License](#license)\n`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (License !== "none") {
+    return `## License
+
+      Licensed under the ${license} license.`;
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  const generateMarkdown = data => {
+    return `# ${data.title}
+    ![Github licence](http://img.shields.io/badge/license-${data.license}-blue.svg)
+  
+    
+    ## Description 
+    ${data.description}
+  
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Features](#features)
+    * [Contributing](#contributing)
+    * [License](#license)
+    * [Badges](#badges)
+    * [Tests](#tests)
+    
+    ## Installation 
+    ${data.install}
+  
+    ## Usage 
+    ${data.usage}
+  
+    ## Features
+    ${data.features}
 
+    ## Contributing
+    ${data.contributing}
+
+    ## License 
+    This project is license under ${data.license}
+
+    ## Badges
+    ${data.badges}
+  
+    ## Tests
+    ${data.test}
+  
 `;
 }
+// use for importing Markdown in index
 
 module.exports = generateMarkdown;
