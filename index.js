f8704ec9702675986f285810674a2a1af7fd5ae5
sheilahanson <slhanson11@live.com>
2024-01-08 13:30:37 -0600

    added more to instalation section

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer"); //const variable can not be reassigned
-const fs = require("fs"); //const file system (fs) to read files on my pc
+const inquirer = require("inquirer"); //npm special software - const variable can not be reassigned
+const fs = require("fs"); //const file system (fs) to read files on my pc 
 const generateMarkdown = require("./utils/generateMarkdown"); //require is built in function, passing location name as argument
 
 // TODO: Create an array of questions for user input
 const questions = [ 
-  //     {
-  //       type: "input", // used for single line responses
-  //       name: "title", //name of configuration
-  //       message: "What is the title of your project?" //question prompt in console.log
-  //     },
-  // {
-  //   type: "editor", //used for multi line responses, saved in notepad
-  //   name: "description",
-  //   message: "Describe your project, the purpose and how it functions."
-  // },
+      {
+        type: "input", // used for single line responses
+        name: "title", //name of configuration
+        message: "What is the title of your project?" //question prompt in console.log
+      },
+  {
+    type: "editor", //used for multi line responses, saved in notepad
+    name: "description",
+    message: "Describe your project, the purpose and how it functions."
+  },
   {
         type: "editor",
         name: "installation",
@@ -35,37 +35,37 @@ const questions = [
           name: "wireframe",
           message: "Add your wireframe link",
         },
-      // {
-      //   type: "editor",
-      //   name: "usage",
-      //   message: "How do you use your application?",
-      // },
-      // {
-      //   type: "editor",
-      //   name: "features",
-      //   message: "What does your project feature?",
-      // },
-  // {
-  //   type: "editor",
-  //   name: "contributing",
-  //   message: "What are contributing sources?",
-  // },
-  // {
-  //   type: "rawlist", //select 1 from choices
-  //   name: "license",
-  //   message: "Select a license for your project",
-  //   choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD3", "GNU3", "none"],
-  // },
-  // {
-  //   type: "editor",
-  //   name: "badges",
-  //   message: "What badges apply?",
-  // },
-  // {
-  //   type: "editor",
-  //   name: "test",
-  //   message: "Add test instructions",
-  // },
+      {
+        type: "editor",
+        name: "usage",
+        message: "How do you use your application?",
+      },
+      {
+        type: "editor",
+        name: "features",
+        message: "What does your project feature?",
+      },
+  {
+    type: "editor",
+    name: "contributing",
+    message: "What are contributing sources?",
+  },
+  {
+    type: "rawlist", //select 1 from choices
+    name: "license",
+    message: "Select a license for your project",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD3", "GNU3", "none"],
+  },
+  {
+    type: "editor",
+    name: "badges",
+    message: "What badges apply?",
+  },
+  {
+    type: "editor",
+    name: "test",
+    message: "Add test instructions",
+  },
   {
     type: "index",
     name: "questions", 
